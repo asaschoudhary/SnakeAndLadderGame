6d079f4c8d8660649725befac1ea57c4ca6ebec8
Ashish <asaschoudhary616@gmail.com>
2020-03-01 15:42:44 +0530

    :ADDED:Check the option of snake ladder and not play

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -4,6 +4,24 @@ echo "Welcome in snake and ladder game"
 #Constant variable
 Numberofplayer=1
 Startposition=0
+Ladder=0
+Snake=1
+NoPlay=3
 #Generate a random value between 1 to 6
 randomRollsDie=$((RANDOM%6+1))
 echo $randomRollsDie
+checkCase=$((RANDOM%3))
+case $checkCase in
+	$Ladder)
+			position=$(( position + rollDie ))
+			echo "Player moves ahead by $rollDie position."
+			;;
+	$Snake)
+			position=$(( position - rollDie ))
+			echo "Player moves behind by $rollDie position."
+			;;
+	$NoPlay)
+			position=$positon
+			echo "Player is on same $Position."
+			;;
+esac
