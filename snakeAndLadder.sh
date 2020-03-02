2cd2d684649eb49a52340cfb7d16e93b1d92d8fa
Ashish <asaschoudhary616@gmail.com>
2020-03-02 10:01:54 +0530

    MODIFIED:Player reaches the winninh position 100.

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -2,26 +2,31 @@
 echo "Welcome in snake and ladder game"
 #Start a game with single player with zero position
 #Constant variable
-Numberofplayer=1
-Startposition=0
-Ladder=0
-Snake=1
-NoPlay=3
+position=0
+ladder=0
+noplay=1
+snake=2
 #Generate a random value between 1 to 6
-randomRollsDie=$((RANDOM%6+1))
-echo $randomRollsDie
-checkCase=$((RANDOM%3))
-case $checkCase in
-	$Ladder)
-			position=$(( position + rollDie ))
-			echo "Player moves ahead by $rollDie position."
-			;;
-	$Snake)
-			position=$(( position - rollDie ))
-			echo "Player moves behind by $rollDie position."
-			;;
-	$NoPlay)
-			position=$positon
-			echo "Player is on same $Position."
-			;;
-esac
+while(($position!=100))
+do
+	randomRollsDie=$((RANDOM%6 + 1))
+	checkCase=$((RANDOM%3))
+	case $checkCase in
+		$ladder)
+				position=$(( $position + $randomRollsDie ))
+				echo "Player moves ahead by position:$position"
+				;;
+		$noplay)
+				position=$position
+				echo "Player is on same $position"
+				;;
+		$snake)
+            position=$(( $position-$randomRollsDie ))
+            echo "Player moves behind by  position:$position"
+            if (( $position<0 ))
+            then
+                  position=0
+				fi
+				;;
+	esac
+done
